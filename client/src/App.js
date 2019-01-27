ea172282a184729daa58ff6634dab2b78e1857a6
Jaggu <khushboojagnani254@gmail.com>
2019-01-27 08:47:17 +0530

    setup the Frontend side

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./App.css";
+
+//Import files
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <Landing />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
